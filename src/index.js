393b1aa607e89605c097248cf8e52174d57cf845
merrybeth <legerumia@gmail.com>
2023-01-06 19:50:38 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = ""
+    n = n.toString()
+    for (let i=n.length; i>=0; i--){
+        result+=n.charAt(i);
+    }
+    return parseInt(result)
 }
